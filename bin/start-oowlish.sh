5b390a28a5032c06de8bdc40e82e6f3e3c8702b6
Dias, Eduardo (ESI) <eduardo.dias@adp.com>
2020-10-18 11:27:50 -0300

    updating startup script

--- bin/start-oowlish.sh
+++ bin/start-oowlish.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+cd /home/ec2-user/oowlish
+python3 -m venv venv
+source venv/bin/activate
+pip3 install -r requirements.txt
 export APP_SECRET_TOKEN=SomeSecretToken
 nohup python3 /home/ec2-user/oowlish/app.py &
