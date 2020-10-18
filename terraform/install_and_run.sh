1903d7edd8e00e08622452fc1f5178a4b5c7b49e
Dias, Eduardo (ESI) <eduardo.dias@adp.com>
2020-10-18 11:28:41 -0300

    updating startup script

--- terraform/install_and_run.sh
+++ terraform/install_and_run.sh
@@ -6,10 +6,6 @@ sudo yum install python36 python36-pip git -y
 
 # Clone and install the app
 git clone https://github.com/edugdias/oowlish.git
-cd oowlish
-python3 -m venv venv
-source venv/bin/activate
-pip3 install -r requirements.txt
 
 # Configure app startup script
 sudo cp ./bin/oowlish /etc/init.d
